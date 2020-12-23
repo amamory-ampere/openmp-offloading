38308b3c6783ffe9c8d4e9f7598521a4cf3b11ec
Alexandre Amory <amamory@gmail.com>
2020-12-23 11:19:59 -0300

    comments to cuda clock example

--- openmp_gpu/clock.c
+++ openmp_gpu/clock.c
@@ -1,40 +1,19 @@
 /*
 EXAMPLE SOURCE : 
 https://forums.developer.nvidia.com/t/reading-globaltimer-register-or-calling-clock-clock64-in-loop-prevent-concurrent-kernel-execution/48600/8
-https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#special-registers-clock64
-https://docs.nvidia.com/cuda/inline-ptx-assembly/index.html
 
-generating Asm PTX code
-https://developer.nvidia.com/blog/cuda-pro-tip-view-assembly-code-correlation-nsight-visual-studio-edition/
-https://stackoverflow.com/questions/20482686/how-to-get-the-assembly-code-of-a-cuda-kernel
-$ nvcc -ptx -o kernel.ptx kernel.cu
-
-.func  (.param .b64 func_retval0) clock64(
-
-)
-{
-        .reg .b64       %rd<3>;
-
-
-        // inline asm
-        mov.u64         %rd1, %clock64;
-        // inline asm
-        mov.b64         %rd2, %rd1;
-        st.param.b64    [func_retval0+0], %rd2;
-        ret;
-}
-
-
-
-
+https://www.openmp.org/spec-html/5.0/openmpsu161.html#x200-9710003.4.2
+https://gcc.gnu.org/onlinedocs/libgomp/omp_005fget_005fwtick.html#omp_005fget_005fwtick
+omp_get_wtick
 
 COMPILATION:
-/usr/local/cuda-10.2/bin/nvcc -ccbin g++ -I../common/inc  -m64 -g -G    -gencode arch=compute_30,code=sm_30 -gencode arch=compute_30,code=compute_30 -o clock2.o -c clock2.cu
-/usr/local/cuda-10.2/bin/nvcc -ccbin g++   -m64 -g -G      -gencode arch=compute_30,code=sm_30 -gencode arch=compute_30,code=compute_30 -o clock2 clock2.o
+gcc clock.c -o clock -fopenmp
 
 THIS EXAMPLE WORKS, WITH SOME LITTLE EXTRA TIME
 */
 #include <stdio.h>
+#include <time.h>
+#include <omp.h>
 
 #define DELAY_VAL 5000000000ULL // about 5 secs
 
@@ -59,14 +38,10 @@ __global__ void child(){
 
 int main(int argc, char* argv[]){
 
-    cudaStream_t st1, st2;
-    cudaStreamCreate(&st1);
-    cudaStreamCreate(&st2);
-
     clock_t ck_start = clock();
     long start = milliseconds();
     long now = 0;
-    child<<<1,1,0,st1>>>();
+    //child<<<1,1,0,st1>>>();
     /*
     if (argc > 1){
         printf("running double kernel\n");
@@ -75,7 +50,7 @@ int main(int argc, char* argv[]){
         }
         //parent<<<1,1,0,st2>>>();
     }*/
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
     printf ("Elapsed: %ld clicks.\n",milliseconds()-start);
     printf ("Kernel: %ld clicks (%f seconds).\n", clock()-ck_start,((float)clock()-ck_start)/CLOCKS_PER_SEC);
     return now;
